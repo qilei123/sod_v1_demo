9df9a76377551478a690d5cfeae032f9c1766c69
qilei123 <qileimail123@gmail.com>
2019-01-17 17:14:07 -0500

    fdad

--- lib/bbox/bbox_transform.py
+++ lib/bbox/bbox_transform.py
@@ -92,8 +92,8 @@ def nonlinear_transform(ex_rois, gt_rois):
 
     targets_dx = (gt_ctr_x - ex_ctr_x) / (ex_widths + 1e-14)
     targets_dy = (gt_ctr_y - ex_ctr_y) / (ex_heights + 1e-14)
-    if ex_widths==0:
-        print 'warning ex_widths:'+str(ex_widths)
+    #if ex_widths==0:
+    print 'warning ex_widths:'+str(ex_widths)
     targets_dw = np.log(gt_widths / ex_widths)
     targets_dh = np.log(gt_heights / ex_heights)
 
