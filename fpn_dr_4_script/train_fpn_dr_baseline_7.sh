1877b266fb1de8913e1000f16f2f0e09b16ea4cc
qilei123 <qileimail123@gmail.com>
2019-01-25 16:53:36 -0500

    fda

---
+++ fpn_dr_4_script/train_fpn_dr_baseline_7.sh
@@ -0,0 +1,3 @@
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data0/qilei_chen/opencv33_install_60/lib:/usr/local/cuda-8.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/qileimail123/opencv32/install/lib:/usr/local/cuda/lib64
+python experiments/fpn/fpn_end2end_train_test.py --cfg experiments/fpn/cfgs/dr_4/resnet_v1_101_dr_trainval_fpn_end2end_ohem_7.yaml
