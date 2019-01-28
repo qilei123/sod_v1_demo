35306b5c10472aca25997f7938732813abdba967
qilei123 <qileimail123@gmail.com>
2019-01-28 13:58:57 -0500

    fad

--- fpn_dr_4_script/test_fpn_dr_baseline_6.sh
+++ fpn_dr_4_script/test_fpn_dr_baseline_6.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/qileimail123/opencv32/install/lib:/usr/local/cuda/lib64
-python experiments/fpn/fpn_test.py --cfg experiments/fpn/cfgs/dr_4/resnet_v1_101_dr_trainval_fpn_end2end_ohem_6.yaml --vis
+python experiments/fpn/fpn_test.py --cfg experiments/fpn/cfgs/dr_4/resnet_v1_101_dr_trainval_fpn_end2end_ohem_6.yaml
