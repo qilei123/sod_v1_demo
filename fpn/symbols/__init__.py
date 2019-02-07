6e41aacdd3acb3b553ad3ef87504230edc5cdc82
qilei123 <qileimail123@gmail.com>
2019-02-07 14:27:16 -0500

    fdad

--- fpn/symbols/__init__.py
+++ fpn/symbols/__init__.py
@@ -3,6 +3,7 @@ import resnet_v1_101_fpn_dcn_rcnn
 import resnet_v1_101_fpn_rcnn_l1
 import resnet_v1_101_fpn_rcnn_l0
 import resnet_v1_101_fpn_dcn_rcnn_l0
+import resnet_v1_101_fpn_dcn_rcnn_l1
 import resnet_unet_rcnn_l0
 import resnet_v1_50_fpn_rcnn_l0
 import vgg_16_fpn_rcnn_l0
