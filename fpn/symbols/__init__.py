4f60e8e4a04b4700c800f342e0287e03e3b6b5b0
qilei123 <qileimail123@gmail.com>
2019-02-12 23:48:49 -0500

    fda

--- fpn/symbols/__init__.py
+++ fpn/symbols/__init__.py
@@ -12,3 +12,4 @@ import resnet_v1_101_fpn_rcnn_l0_focal
 import resnet_v1_101_fpn_dcn_rcnn_l0_focal
 import resnet_v1_50_fpn_rcnn_l0_focal
 import resnet_v1_101_fpn_dcn_rcnn_l2_focal
+import vgg_16_fpn_rcnn_l0_focal
