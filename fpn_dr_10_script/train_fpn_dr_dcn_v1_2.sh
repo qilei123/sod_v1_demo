833495f8d3a617f3e99a7ebbf065332c14bae76a
qilei123 <qileimail123@gmail.com>
2019-03-08 15:21:52 -0500

    fdafd

--- fpn_dr_10_script/train_fpn_dr_dcn_v1_2.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data0/qilei_chen/opencv33_install_66/lib:/usr/local/cuda-8.0/lib64
-python experiments/fpn/fpn_end2end_train_test.py --cfg experiments/fpn/cfgs/resnet_v1_101_dr_trainval_fpn_dcn_end2end_ohem_2.yaml
