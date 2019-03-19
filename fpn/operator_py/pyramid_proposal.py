0dea2de567e3ec3d0c5a22e60257f251264ffcb8
qilei123 <qileimail123@gmail.com>
2019-03-19 17:50:18 -0400

    fda

--- fpn/operator_py/pyramid_proposal.py
+++ fpn/operator_py/pyramid_proposal.py
@@ -175,8 +175,8 @@ class PyramidProposalOperator(mx.operator.CustomOp):
             stride = int(s)
             sub_anchors = generate_anchors(base_size=stride, scales=self._scales, ratios=self._ratios)
             #print "cls_prob_dict['stride' + str(s)].shape:"+str(cls_prob_dict['stride' + str(s)].shape)
-            #scores = cls_prob_dict['stride' + str(s)].asnumpy()[:, self._num_anchors:, :, :]
-            scores = cls_prob_dict['stride' + str(s)].asnumpy()
+            scores = cls_prob_dict['stride' + str(s)].asnumpy()[:, self._num_anchors:, :, :]
+            #scores = cls_prob_dict['stride' + str(s)].asnumpy()
             
             #print "scores.shape:"+str(scores.shape)
             bbox_deltas = bbox_pred_dict['stride' + str(s)].asnumpy()
