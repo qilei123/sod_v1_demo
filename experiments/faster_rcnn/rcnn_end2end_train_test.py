6362480a4b9fdd15f5ffd01c9a2493a33c791d62
qilei123 <qileimail123@gmail.com>
2019-03-21 00:29:52 -0400

    fda

--- experiments/faster_rcnn/rcnn_end2end_train_test.py
+++ experiments/faster_rcnn/rcnn_end2end_train_test.py
@@ -12,8 +12,9 @@ os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
 os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
 #os.environ['MXNET_ENGINE_TYPE'] = 'NaiveEngine'
 this_dir = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(this_dir, '..', '..', 'faster_rcnn'))
-
+#sys.path.insert(0, os.path.join(this_dir, '..', '..', 'faster_rcnn'))
+mxnet_path = os.path.join(this_dir, '../external/incubator-mxnet/python/')
+sys.path.insert(0, mxnet_path)
 import train_end2end
 import test
 
