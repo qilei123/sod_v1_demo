3b2888cc756ac1c3295c4277027db492cf4f6606
qilei123 <qileimail123@gmail.com>
2019-04-04 18:54:09 -0400

    fda

--- experiments/faster_rcnn/rcnn_demo_kaggle_0.py
+++ experiments/faster_rcnn/rcnn_demo_kaggle_0.py
@@ -4,7 +4,6 @@
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Guodong Zhang
 # --------------------------------------------------------
-
 import os
 import sys
 import json
@@ -32,17 +31,11 @@ def draw_all_boxes(img_path,boxes_result):
         category_count[int(rbox['label'])-1]+=1
     return img
 def predict_for_stage(stage):
-    
+    fd = faster_detector()
     cfg_path = 'experiments/faster_rcnn/cfgs/resnet_v1_101_dr_trainval_rcnn_end2end_1.yaml'
     prefix = '/home/qileimail123/data0/RetinaImg/BostonAI4DB7/faster_baseline1/resnet_v1_101_dr_trainval_rcnn_end2end_1/train2014/rcnn_coco'
-    fd = faster_detector()
     fd.init_predictor(cfg_path,prefix,10)
-    fd0 = faster_detector()
-    fd0.init_predictor(cfg_path,prefix,10)
-    fd1 = faster_detector()
-    fd1.init_predictor(cfg_path,prefix,10)
-
-
+    
     data_path = '/home/qileimail123/data0/RetinaImg/kaggle_DB'
     train_set_file = open(data_path+'/trainLabels.csv')
     train_line = train_set_file.readline()
@@ -65,9 +58,6 @@ def predict_for_stage(stage):
 
     category_id = stage
     count = 0
-    img_group = []
-    stages = []
-    img_names = []
     while train_line:
         split_line = train_line.split(',')
         img_stage = int(split_line[1])
@@ -78,67 +68,21 @@ def predict_for_stage(stage):
         count+=1
         print count
         img_path = data_path+'/'+train_folder+'/'+split_line[0]+extends
-        if not os.path.exists(data_path+'/'+train_save_folder+'/'+str(img_stage)):
-            os.makedirs(data_path+'/'+train_save_folder+'/'+str(img_stage))
         #print img_path
         #print img_stage
-        
-        if count%3==0:
-            tsk = []
-            t1 = threading.Thread (target =  fd.prediction,args = (img_path,))
-            t1.start()
-            tsk.append(t1)
-            t2 = threading.Thread (target =  fd0.prediction,args = (img_path,))
-            t2.start()
-            tsk.append(t2)
-            t3 = threading.Thread (target =  fd1.prediction,args = (img_path,))
-            t3.start()
-            tsk.append(t3)
-            for tt in tsk:
-                tt.join()
-                        
-            train_result1 = {
-                    'img':img_names[0]+extends,
-                    'stage':stages[0],
-                    'result':fd.getResult()
-                    }
-            img_with_boxes1 = draw_all_boxes(img_group[0],fd.getResult())
-            cv2.imwrite(data_path+'/'+train_save_folder+'/'+str(stages[0])+'/'+img_names[0]+extends,img_with_boxes1)
-            del img_with_boxes1
-            gc.collect()
-            train_results['results_list'].append(train_result1)
-
-            train_result2 = {
-                    'img':img_names[1]+extends,
-                    'stage':stages[1],
-                    'result':fd0.getResult()
-                    }
-            img_with_boxes2 = draw_all_boxes(img_group[1],fd0.getResult())
-            cv2.imwrite(data_path+'/'+train_save_folder+'/'+str(stages[1])+'/'+img_names[1]+extends,img_with_boxes2)
-            del img_with_boxes2
-            gc.collect()
-            train_results['results_list'].append(train_result2)
-
-            train_result3 = {
-                    'img':img_names[2]+extends,
-                    'stage':stages[2],
-                    'result':fd1.getResult()
-                    }
-            img_with_boxes3 = draw_all_boxes(img_group[3],fd1.getResult())
-            cv2.imwrite(data_path+'/'+train_save_folder+'/'+str(stages[2])+'/'+img_names[2]+extends,img_with_boxes3)
-            del img_with_boxes3
-            gc.collect()
-            train_results['results_list'].append(train_result3)
-
-            img_group=[]
-            stages = []
-            img_names = []
-        else:
-            img_group.append(img_path)
-            stages.append(img_stage)
-            img_names.append(split_line[0])
-
-
+        boxes_result = fd.prediction(img_path)
+        train_result = {
+                'img':split_line[0]+extends,
+                'stage':img_stage,
+                'result':boxes_result
+                }
+        img_with_boxes = draw_all_boxes(img_path,boxes_result)
+        if not os.path.exists(data_path+'/'+train_save_folder+'/'+str(img_stage)):
+            os.makedirs(data_path+'/'+train_save_folder+'/'+str(img_stage))
+        cv2.imwrite(data_path+'/'+train_save_folder+'/'+str(img_stage)+'/'+split_line[0]+extends,img_with_boxes)
+        del img_with_boxes
+        gc.collect()
+        train_results['results_list'].append(train_result)
         train_line = train_set_file.readline()
     train_results_json = json.dumps(train_results)
     with open(data_path+'/train_results_'+str(category_id)+'.json', 'w') as json_file:
