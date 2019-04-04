7e60177cdedbeaec366bf2c5c69a6dd39596cd0d
qilei123 <qileimail123@gmail.com>
2019-04-04 17:00:18 -0400

    fda

--- faster_rcnn/core/tester.py
+++ faster_rcnn/core/tester.py
@@ -234,7 +234,10 @@ def pred_eval(predictor, test_data, imdb, cfg, vis=False, thresh=1e-3, logger=No
             boxes_this_image = [[]] + [all_boxes[j][idx+delta] for j in range(1, num_classes)]
             if vis:
                 vis_all_detection(data_dict['data'].asnumpy(), boxes_this_image, classes, scales[delta], cfg)
+            
             results = get_json_result(data_dict['data'].asnumpy(), boxes_this_image, classes, scales[delta], cfg)
+            del data_dict,boxes_this_image
+            gc.collect()
 
         idx += test_data.batch_size
         t3 = time.time() - t
