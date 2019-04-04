bfcdea59210fb30385f1c99761a764ae55f5f422
qilei123 <qileimail123@gmail.com>
2019-04-04 17:38:34 -0400

    fda

--- faster_rcnn/function/test_rcnn.py
+++ faster_rcnn/function/test_rcnn.py
@@ -174,8 +174,10 @@ class detector:
         roidb.append(roi_rec)
         # get test data iter
         test_data = TestLoader(roidb, self.cfg, batch_size=len(self.ctx), shuffle=self.shuffle, has_rpn=self.has_rpn)
-        result =  pred_eval(self.predictor, test_data, None, self.cfg, vis=self.vis, 
-                    ignore_cache=self.ignore_cache, thresh=self.thresh, logger=self.logger)
+        result =  {'results':[]}
+        #pred_eval(self.predictor, test_data, None, self.cfg, vis=self.vis, 
+        #            ignore_cache=self.ignore_cache, thresh=self.thresh, logger=self.logger)
+        
         del test_data,img
         gc.collect()
         return result
