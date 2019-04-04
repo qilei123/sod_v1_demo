d90896d85f833160136108e8bc1a9bf5a64b6800
qilei123 <qileimail123@gmail.com>
2019-04-04 15:59:25 -0400

    fda

--- experiments/faster_rcnn/rcnn_demo_kaggle_0.py
+++ experiments/faster_rcnn/rcnn_demo_kaggle_0.py
@@ -111,5 +111,5 @@ if __name__ == "__main__":
     #predict_for_stage(0)
     for i in range(5):
         #thread.start_new_thread(predict_for_stage,(i,))
-        my_thread = threading.Thread(target = predict_for_stage,(i,))
+        my_thread = threading.Thread(target = predict_for_stage,args=(i,))
         my_thread.start()
