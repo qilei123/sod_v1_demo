b8d76aa2c734e987d7b7e4f17eb20bb5e3520703
qilei123 <qileimail123@gmail.com>
2019-04-05 18:06:39 -0400

    fda

--- experiments/faster_rcnn/rcnn_demo_kaggle.py
+++ experiments/faster_rcnn/rcnn_demo_kaggle.py
@@ -124,9 +124,9 @@ if __name__ == "__main__":
     predict_for_stage(0)
     print category_count
 '''
-    '''
+'''
     for i in range(5):
         #thread.start_new_thread(predict_for_stage,(i,))
         my_thread = threading.Thread(target = predict_for_stage,args=(i,))
         my_thread.start()
-    '''
+'''
